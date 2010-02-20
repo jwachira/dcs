dfcb3c4f9bb555d4234780c29a2ed96310170c61
Mark Nutini <mnutini@gmail.com>
2010-02-19 21:53:10 -0800

    updated base deploy from current wested_cap, also includes latest hoptoad addition.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,80 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+#############################################################
+# Set Basics
+#############################################################
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :application, ""
+set :user, ""
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+# this require needs to be run after the above variable sets as they are required by the files we load
+require File.join(File.dirname(__FILE__), "../vendor/plugins/wested_cap/lib/wested/wested_cap")
+
+#############################################################
+# Set Up for Production Environment
+#############################################################
+
+task :production do
+  prep_production
+  set :domain, ""
+
+  role :web, ""
+  role :app, ""
+  role :db , "", :primary => true
+end
+
+#############################################################
+# Set Up for Development Environment
+#############################################################
+
+task :dev do
+  set :staging_type, "dedicated"
+  set(:rails_env ) { "development" }
+  prep_staging
+  set :domain, "calipers.virtual"
+  role :web, "10.211.55.13"
+  role :app, "10.211.55.13"
+  role :db , "10.211.55.13", :primary => true
+end
+
+#############################################################
+# Set Up for Staging Environment
+#############################################################
+
+task :staging do
+  prep_staging
+  # We use a single server for all projects for staging.
+  # You can set your default staging ipaddresses and url in
+  # defaults/staging_defaults in the plugin
+  #
+  # Or you can override them here.
+end
+
+
+#############################################################
+# Project Specific Symlinks
+#############################################################
+
+# these are project specific but still reside in shared
+# eg 'foo/bar' => 'bar'
+# creates the command "rm -rf #{release_path}/bar && ln -s #{shared_path}/foo/bar #{release_path}/bar"
+
+#set :custom_symlinks, {}
+
+# these are project specific but are the strange guys - they need full from paths
+# eg '/var/www/apps/project/foo' => 'foo'
+# creates the command "rm -rf #{release_path}/foo && ln -s /var/www/apps/project/foo #{release_path}/foo"
+
+#set :other_symlinks, {}
+
+#############################################################
+# Project Specific Tasks
+#############################################################
+
+# place other tasks here and link in via
+#after "passenger:restart", "project_namespace:new_task_name"
+
+Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'hoptoad_notifier-*')].each do |vendored_notifier|
+  $: << File.join(vendored_notifier, 'lib')
+end
+
+require 'hoptoad_notifier/capistrano'
