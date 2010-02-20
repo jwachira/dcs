efa270537758572e2400cf880436533fda9c4bbd
Mark Nutini <mnutini@gmail.com>
2010-02-19 16:46:01 -0800

    changed flash messages from notice to error where needed. Changed flash to flash.now on messages that process within same page load.

--- app/controllers/users/password_controller.rb
+++ app/controllers/users/password_controller.rb
@@ -1,6 +1,5 @@
 class Users::PasswordController < ApplicationController
   before_filter :require_object_is_current_user
-  skip_before_filter :require_manager
   
   def edit
     @user = object
@@ -12,19 +11,19 @@ class Users::PasswordController < ApplicationController
       @user.password = params[:user][:password]
       @user.password_confirmation = params[:user][:password_confirmation]
       if params[:user][:password].blank?
-        flash[:notice] = "You must enter a new password."
+        flash.now[:error] = "You must enter a new password."
         render :action => :edit
       else
         if @user.save
           flash[:notice] = "Password successfully changed."
           redirect_to user_url(@user)
         else
-          flash[:notice] = "There was a problem changing your password."
+          flash.now[:error] = "There was a problem changing your password."
           render :action => :edit
         end
       end
     else
-      flash[:notice] = "The current password you entered is incorrect."
+      flash.now[:error] = "The current password you entered is incorrect."
       render :action => :edit
     end
   end
