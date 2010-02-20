378447a83a0336ecd057bbe42dce9fbe6e127a2c
Mark Nutini <mnutini@gmail.com>
2010-02-19 21:49:57 -0800

    base time formats

---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,11 @@
+ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:default] =
+ '%m/%d/%Y'
+ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:url] =
+ '%Y-%m-%d'
+ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:without_year] =
+ '%m/%d'
+ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:default] =
+ '%I:%M %p'
+ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:without_meridian] =
+ '%I:%M'
+ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:date_time] = '%m/%d/%Y at %I:%M %p'
