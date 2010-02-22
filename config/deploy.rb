c37e24144830a18ec67c0e9825060f742e693781
Mark Nutini <mnutini@gmail.com>
2010-02-22 15:45:23 -0800

    removed hoptoad generated initializer and deploy code. It's better to run the hoptoad generator script and get the most current version.

--- config/deploy.rb
+++ config/deploy.rb
@@ -73,8 +73,3 @@ end
 # place other tasks here and link in via
 #after "passenger:restart", "project_namespace:new_task_name"
 
-Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'hoptoad_notifier-*')].each do |vendored_notifier|
-  $: << File.join(vendored_notifier, 'lib')
-end
-
-require 'hoptoad_notifier/capistrano'
