f3b5bdb93951efc0b9adf0dd14f6a5d01ceb155b
Mark Nutini <mnutini@gmail.com>
2010-02-23 14:38:23 -0800

    added roles support to User for declarative_authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,24 @@ class User < ActiveRecord::Base
   acts_as_authentic
   model_stamper
   stampable
-
+  
+  ROLES = ['Admin','User']
+  
   attr_protected :password
   validates_length_of :password, :minimum => 6, :allow_nil => true
   validates_format_of :password, :with => /[a-z]/i, :allow_nil => true, :message => "must include a letter"
   validates_format_of :password, :with => /[0-9]/, :allow_nil => true, :message => "must include a number"
+
+
+  # The necessary method for the plugin to find out about the role symbols
+  # Roles returns e.g. [:admin]
   
+  def role_symbols
+    @role_symbols ||= (!role.nil?) ? [role.downcase.to_sym] : []
+  end
+
+  # End of declarative_authorization code
+    
   def full_name
     "#{first_name} #{last_name}"
   end
