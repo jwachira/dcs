84c6d987408aa489c2b72ddfeededcdc4862d6c2
Mark Nutini <mnutini@gmail.com>
2010-02-23 14:58:44 -0800

    annotated User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,29 @@
+=begin Schema Information
+
+ Table name: users
+
+  id                :integer(4)      not null, primary key
+  first_name        :string(255)
+  last_name         :string(255)
+  crypted_password  :string(255)
+  password_salt     :string(255)
+  persistence_token :string(255)
+  perishable_token  :string(255)
+  login_count       :integer(4)      default(0), not null
+  email             :string(255)     default(""), not null
+  role              :string(20)
+  last_request_at   :datetime
+  last_login_at     :datetime
+  current_login_at  :string(255)
+  last_login_ip     :string(255)
+  current_login_ip  :string(255)
+  creator_id        :integer(4)
+  updater_id        :integer(4)
+  created_at        :datetime
+  updated_at        :datetime
+
+=end Schema Information
+
 class User < ActiveRecord::Base
   acts_as_authentic
   model_stamper
