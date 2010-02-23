7e5bc3f5aef3524421a7158c78cdad8bfec36820
Mark Nutini <mnutini@gmail.com>
2010-02-23 14:48:35 -0800

    Hoptoad gem initially disabled since there is no configuration. Removed plugin load order, resource_controller has been removed from the baseapp so there is no need to order plugin loading.

--- config/environment.rb
+++ config/environment.rb
@@ -23,22 +23,16 @@ Rails::Initializer.run do |config|
   # Specify gems that this application depends on. 
   # They can then be installed with "rake gems:install" on new installations.
   # You have to specify the :lib option for libraries, where the Gem name (sqlite3-ruby) differs from the file itself (sqlite3)
-  # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  # config.gem "aws-s3", :lib => "aws/s3"
-  #config.gem "authlogic"
   config.gem "authlogic"
   config.gem 'formtastic'
   config.gem 'declarative_authorization'
   config.gem 'paperclip'
-  config.gem 'hoptoad_notifier'
+  # config.gem 'hoptoad_notifier'
   
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-  config.plugins = [ :all, :resource_controller]
   
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
