af94e7c2ed74a4ec48b5f72fad5453253fc65295
Mark Nutini <mnutini@gmail.com>
2010-02-23 15:04:05 -0800

    updated root route to point to special instead of home

--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :account, :controller => "users"
   
   map.resource :user_session
-  map.root :controller => "home", :action => "index"
+  map.root :controller => "special", :action => "index"
   map.login  'login',  :controller => "user_sessions", :action => "new"
   map.logout 'logout', :controller => "user_sessions", :action => "destroy"
   map.resources :users, :member => {:delete => :get} do |user|
