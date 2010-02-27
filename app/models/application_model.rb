b087e8d9966368fbe8395f1d27085e7e143709d5
Andrew Carpenter <acarpen@wested.org>
2010-02-27 08:44:41 -0800

    make applicationmodel abstract

--- app/models/application_model.rb
+++ app/models/application_model.rb
@@ -1,5 +1,5 @@
 class ApplicationModel < ActiveRecord::Base
+  self.abstract_class = true
   # custom ActiveRecord methods should go here, and all application models should inherit from
   #   this instead of ActiveRecord::Base
-  
 end
