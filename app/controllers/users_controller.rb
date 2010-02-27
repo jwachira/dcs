8ecd2a3487e5f018a7467f16cf2cce80e19e937d
Andrew Carpenter <acarpen@wested.org>
2010-02-27 08:51:26 -0800

    ditch XML support in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,20 @@
 class UsersController < ApplicationController
   filter_access_to :all
   filter_access_to :edit, :update, :attribute_check => true
+
   # GET /users
-  # GET /users.xml
   def index
     @users = User.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @users }
-    end
   end
 
   # GET /users/1
-  # GET /users/1.xml
   def show
     @user = User.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @user }
-    end
   end
 
   # GET /users/new
-  # GET /users/new.xml
   def new
     @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @user }
-    end
   end
 
   # GET /users/1/edit
@@ -40,7 +23,6 @@ class UsersController < ApplicationController
   end
 
   # POST /users
-  # POST /users.xml
   def create
     @user = User.new
     @user.email = params[:user][:email]
@@ -52,20 +34,15 @@ class UsersController < ApplicationController
       @user.role = params[:user][:role]
     end
     
-    respond_to do |format|
-      if @user.save
-        flash[:notice] = 'User was successfully created.'
-        format.html { redirect_to(@user) }
-        format.xml  { render :xml => @user, :status => :created, :location => @user }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
-      end
+    if @user.save
+      flash[:notice] = 'User was successfully created.'
+      redirect_to(@user)
+    else
+      render :action => "new"
     end
   end
 
   # PUT /users/1
-  # PUT /users/1.xml
   def update
     @user = User.find(params[:id])
     @user.email = params[:user][:email]
@@ -77,38 +54,24 @@ class UsersController < ApplicationController
       @user.role = params[:user][:role]
     end
     
-    respond_to do |format|
-      if @user.save
-        flash[:notice] = 'User was successfully updated.'
-        format.html { redirect_to(@user) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
-      end
+    if @user.save
+      flash[:notice] = 'User was successfully updated.'
+      redirect_to(@user)
+    else
+      render :action => "edit"
     end
   end
   
   # GET /users/1/delete
-  # GET /users/1/delete.xml
   def delete
     @user = User.find(params[:id])
-
-    respond_to do |format|
-      format.html # delete.html.erb
-      format.xml  { render :xml => @user }
-    end
   end
   
   # DELETE /users/1
-  # DELETE /users/1.xml
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(users_url) }
-      format.xml  { head :ok }
-    end
+    
+    redirect_to(users_url)
   end
 end
