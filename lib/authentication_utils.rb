b9ec717bbb82065c18e13ba61162769fb3789c42
Andrew Carpenter <acarpen@wested.org>
2010-02-27 08:56:07 -0800

    tidy auth utils

--- lib/authentication_utils.rb
+++ lib/authentication_utils.rb
@@ -6,17 +6,15 @@ module AuthenticationUtils
   end
   
   def redirect_back_or_default(default)
-    redirect_to(session[:return_to] || params[:return_to] || default)
+    redirect_to(session[:return_to] || default)
     session[:return_to] = nil
   end
 
   def current_user_session
-    return @current_user_session if defined?(@current_user_session)
-    @current_user_session = UserSession.find
+    @current_user_session ||= UserSession.find
   end
 
   def current_user
-    return @current_user if defined?(@current_user)
-    @current_user = current_user_session && current_user_session.record
+    @current_user ||= current_user_session && current_user_session.record
   end
 end
