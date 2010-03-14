7dbbaf32e6e439b75d9bea6f3928a12220fb15f0
Andrew Carpenter <acarpen@wested.org>
2010-03-14 10:06:10 -0700

    add calendar widget

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// Calendar date-picker widget
+$("li.calendar input").datepicker({
+  yearRange: '-10:+4',
+  dateFormat: 'mm/dd/yy',
+  showOn: 'button',
+  buttonImage: '/images/calendar.png',
+  buttonImageOnly: true
+});
